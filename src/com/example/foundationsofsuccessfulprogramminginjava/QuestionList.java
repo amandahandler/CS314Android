77797a9daa4708e72ffdd0bd07f8abb2ef5bd7f9
Thaddeus Johnston <johnston@hams-fork.cs.colostate.edu>
2013-04-30 12:21:44 -0600

    Added a question

--- src/com/example/foundationsofsuccessfulprogramminginjava/QuestionList.java
+++ src/com/example/foundationsofsuccessfulprogramminginjava/QuestionList.java
@@ -3,5 +3,6 @@ package com.example.foundationsofsuccessfulprogramminginjava;
 public class QuestionList {
 
 	static Question q1 = new Question("What is the hot-key for copying text?", "CTRL-Z", "CTRL-X", "CTRL-C", "CTRL-Y", 2);
+	static Question q2 = new Question("What is the hot-key for copying text?", "CTRL-Z", "CTRL-X", "CTRL-C", "CTRL-Y", 2);
 
 }
